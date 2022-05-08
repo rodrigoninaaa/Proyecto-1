53204c085c95fd060ca2454d91851b98456f3948
Rodrigo Nina <ninarodrigo6@gmail.com>
2022-05-08 17:08:39 -0500

    segunda version

--- ejemplo.cpp
+++ ejemplo.cpp
@@ -6,6 +6,7 @@ int main()
 {
     cout<<"Hola mundo"<<endl;
     cout<<"kjsahdjkashd iasdjaisjdias isadhjiasojdioas asidhaskdias jashdasd"<<endl;
+    cout<<"Feliz dia de la mama"<<endl;
 
     return 0;
 }
