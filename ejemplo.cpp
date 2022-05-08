0e43dc5e51bd2d8e65879e823496d8b6cec50b7d
Rodrigo Nina <ninarodrigo6@gmail.com>
2022-05-08 16:46:47 -0500

    Primera version hola mundo

---
+++ ejemplo.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hola mundo"<<endl;
+    cout<<"kjsahdjkashd iasdjaisjdias isadhjiasojdioas asidhaskdias jashdasd"<<endl;
+
+    return 0;
+}
